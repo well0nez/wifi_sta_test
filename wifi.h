ccaf4e4fb8c937457917f35a854852330ef204f8
well0nez <inopuli@web.de>
2022-09-18 15:39:54 +0200

    Update wifi.h

--- wifi.h
+++ wifi.h
@@ -1,3 +1,4 @@
+#define RDA_BT_IOCTL_MAGIC						'u'
 #define RDA_WIFI_POWER_ON_IOCTL                            _IO(RDA_BT_IOCTL_MAGIC ,0x10)
 #define RDA_WIFI_POWER_OFF_IOCTL                           _IO(RDA_BT_IOCTL_MAGIC ,0x11)
 #define RDA_WIFI_STA_MODE_IOCTL                _IO(RDA_BT_IOCTL_MAGIC ,0x20)
