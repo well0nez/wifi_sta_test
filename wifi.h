75541c04743fedf3b3509037445c4e4961fddbdc
well0nez <inopuli@web.de>
2022-09-18 15:32:56 +0200

    Create wifi.h

---
+++ wifi.h
@@ -0,0 +1,4 @@
+#define RDA_WIFI_POWER_ON_IOCTL                            _IO(RDA_BT_IOCTL_MAGIC ,0x10)
+#define RDA_WIFI_POWER_OFF_IOCTL                           _IO(RDA_BT_IOCTL_MAGIC ,0x11)
+#define RDA_WIFI_STA_MODE_IOCTL                _IO(RDA_BT_IOCTL_MAGIC ,0x20)
+#define RDA_WIFI_SOFTAP_MODE_IOCTL             _IO(RDA_BT_IOCTL_MAGIC ,0x21)
