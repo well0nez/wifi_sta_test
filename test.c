6f116dde07fd887c803a2b9525c829f006284b3b
well0nez <inopuli@web.de>
2022-09-18 15:40:26 +0200

    Create test.c

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <termios.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <linux/types.h>
+#include "wifi.h"
+
+#define BT_CTRL "/dev/rdacombo"
+
+
+int main(int argc, char *argv[])
+{
+  fd = open(BT_CTRL, O_RDWR);
+  if (fd < 0) {
+		return fd;
+  } else {
+	  ioctl(fd, RDA_WIFI_POWER_OFF_IOCTL);
+    close(fd); 
+  }
+  return;
+}
